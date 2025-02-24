3e2a48b745aa8cfd72054c65bdd3ea254850d112
24LP2564 24LP2564 <24LP2564@G000-CC0000.local>
2025-02-24 17:43:53 +0800

    lol

---
+++ src/app/hey.js
@@ -0,0 +1,5 @@
+"use client";
+import { useState, useEffect } from "react";
+import styles from "./page.module.css";
+
+export default function Home() {}
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,56 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+
+const formatTime = (time) => {
+  const sec = time % 60;
+  const min = Math.floor((time % 3600) / 60);
+  const hour = Math.floor(time / 3600);
+  return `${hour.toString().padStart(2, "0")} : ${min
+    .toString()
+    .padStart(2, "0")} : ${sec.toString().padStart(2, "0")}`;
+};
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [time, setTime] = useState(0);
+  const [isWorking, setIsWorking] = useState(false);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    if (!isWorking) return;
+
+    const id = setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [isWorking]);
+
+  const handleStart = () => setIsWorking(true);
+  const handleStop = () => setIsWorking(false);
+  const handleReset = () => {
+    setIsWorking(false);
+    setTime(0);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.counter}>
+        <div className={styles.title}>Online Timer & Stopwatch</div>
+        <div className={styles.timer}>{formatTime(time)}</div>
+        <div className={styles.buttons}>
+          <button className={styles.button17} onClick={handleStart}>
+            Start
+          </button>
+          <button className={styles.button17} onClick={handleStop}>
+            Stop
+          </button>
+          <button className={styles.button17} onClick={handleReset}>
+            Reset
+          </button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
